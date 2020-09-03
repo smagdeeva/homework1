08cf8a3beeb808d2291767e5faeb2d8cc5c81364
Magdeeva <Sabina.Magdeeva@X5.ru>
2020-09-03 11:24:01 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import java.util.Scanner;
+
+/*public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        System.out.println(value % 10);
+    }
+} */
+/*public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите целое трехзначное число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        System.out.println((value % 10) + (value / 10 % 10)+ (value/100));
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите целое  число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        if (value>0)
+            ++value;
+        System.out.println(value);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите целое  число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        if (value > 0) {
+            ++value;
+        } else if (value < 0) {
+            value = value - 2;
+        } else if (value == 0) {
+            value = 10;
+        }
+
+        System.out.println(value);
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите три целых числа: ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if(a<=b&&a<=c) {
+            System.out.println(a);
+        } else if(b<=a&&b<=c) {
+            System.out.println(b);
+        } else {
+            System.out.println(c);
+        }
+
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите целое число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+
+        if((value % 2 == 0) && (value < 0)) {
+            System.out.println("Отрицательное четное число " + value);
+        } else if((value % 2 == 1)&& (value > 0) ){
+            System.out.println("Положительное нечетное число "+ value);
+        } else if (value == 0){
+            System.out.println("Нулевое число "+ value);
+        }
+
+    }
+}
+*/
